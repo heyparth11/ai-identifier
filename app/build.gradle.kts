1ef17bd73dc007c76bb6a64f91eb0619b3f5a070
heyparth11 <nakulbhardwaj9760@gmail.com>
2026-06-18 23:18:31 +0530

    release pipeline

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,24 @@
+
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
+}
+
+
+val keystorePropertiesFile =
+    rootProject.file("key.properties")
+
+val keystoreProperties = Properties()
+
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(
+        FileInputStream(keystorePropertiesFile)
+    )
 }
 
 android {
@@ -23,15 +39,42 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+    signingConfigs {
+        create("release") {
+            storeFile =
+                rootProject.file(keystoreProperties["storeFile"] as String)
+
+            storePassword =
+                keystoreProperties["storePassword"] as String
+
+            keyAlias =
+                keystoreProperties["keyAlias"] as String
+
+            keyPassword =
+                keystoreProperties["keyPassword"] as String
         }
     }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+
+            proguardFiles(
+                getDefaultProguardFile(
+                    "proguard-android-optimize.txt"
+                ),
+                "proguard-rules.pro"
+            )
+
+            signingConfig =
+                signingConfigs.getByName("release")
+        }
+        debug {
+            signingConfig =
+                signingConfigs.getByName("release")
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
